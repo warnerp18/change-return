fed93be11ca5252e4ccd8064d26a2b634636add5
phil <warnerp18@gmail.com>
2020-11-25 14:13:19 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function getChange(amount, changeArray) {
+  if(amount < 0 || Number.isInteger(amount) === false) {
+    console.error(`amount passed must be a number greater than or equal to 0, passed: ${amount}`)
+    throw new Error(`amount passed must be a number greater than or equal to 0, passed: ${amount}`);
+  }
+  if(!Array.isArray(changeArray) || changeArray.length <= 0) {
+    console.error(`changeArray passed must be an array of integers and cannot be empty`)
+    throw new Error(`changeArray passed must be an array of integers and cannot be empty`);
+  }
+
+  let currentTotal = 0;
+  let amountRemainingToGive = amount - currentTotal;
+  let changeObj = {}
+  // sort array in case change not given in highest to lowest
+  changeArray = changeArray.sort((a,b) => a > b ? -1: 1 )
+
+  changeArray.forEach(change => {
+    if(!Number.isInteger(change)) {
+      console.error(`change must be an array of integers`);
+      throw new Error(`change must be an array of integer`);
+    }
+    if(amountRemainingToGive === 0) {
+      return;
+    }
+
+    const coins =  Math.floor(amountRemainingToGive/change);
+
+    if(coins > 0) {
+      changeObj[change] = coins;
+      amountRemainingToGive-= (coins*change)
+    }
+  });
+
+  if(amountRemainingToGive > 0) {
+    throw new Error(`Unable to give correct change amount back: ${amountRemainingToGive} remians`)
+
+  }
+  return changeObj;
+}
+
+
+
+
 function App() {
   return (
     <div className="App">
@@ -22,4 +66,4 @@ function App() {
   );
 }
 
-export default App;
+export {App, getChange };
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { getChange } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+beforeEach(() => {
+  jest.spyOn(console, 'error').mockImplementation(() => {});
 });
+
+test('calculates correct amount of change to return using standard us currency amounts', () => {
+  expect(getChange(99, [25, 10, 5, 1])).toEqual({25: 3, 10: 2, 1: 4})
+  expect(getChange(21, [2, 34, 15, 51])).toEqual({15: 1, 2: 3})
+});
+
+test('calculates correct amount of change to return using random currency amounts', () => {
+  expect(getChange(99, [2, 34, 15, 51])).toEqual({51: 1, 34: 1, 2: 7})
+});
+
+test('calculates correct amount of change to return when given unsorted array of change', () => {
+    expect(getChange(99, [10, 1, 25, 5])).toEqual({25: 3, 10: 2, 1: 4})
+});
+
+test('throws error if amount of change to return is 0 or less', () => {
+  expect(() => getChange(-99, [25, 10, 5, 1])).toThrow()
+});
+   
+test('throws error if array of change is empty', () => {
+  expect(() => getChange(-99, [])).toThrow()
+});
+
+test('throws error if array of change has a value that is not an integer', () => {
+  expect(() => getChange(-99, [25, 10, '5', 1])).toThrow('amount passed must be a number greater than or equal to 0, passed: -99')
+});
+
+test('throws error if unable to give exact amount of change back', () => {
+  expect(() => getChange(22, [2, 34, 15, 51])).toThrow('Unable to give correct change amount back: 1 remians');
+});
+
